b1db543b45608180381402174a6ec866aa4aa234
Nathan Bouquet <114756925+NathanBouq@users.noreply.github.com>
2023-11-30 13:21:11 +0100

    Added 2 buttons tried to make them work but doesn't

--- src/main/java/com/rummikub/gui/GameScreen.java
+++ src/main/java/com/rummikub/gui/GameScreen.java
@@ -32,6 +32,15 @@ class GameScreen extends Pane {
         endTurn.setLayoutX(RackGUI.RACK_X + RackGUI.RACK_WIDTH + 50);
         endTurn.setLayoutY(RackGUI.RACK_Y + RackGUI.RACK_HEIGHT / 2 - 50);
 
+        Button orderByStairs = new HoverButton("Order By Stairs");
+        orderByStairs.setPrefSize(400, 100);
+        orderByStairs.setLayoutX(RackGUI.RACK_X + RackGUI.RACK_WIDTH );
+        orderByStairs.setLayoutY(RackGUI.RACK_Y + RackGUI.RACK_HEIGHT / 2 - 250);
+
+        Button orderByGroup = new HoverButton("Order By Group");
+        orderByGroup.setPrefSize(400, 100);
+        orderByGroup.setLayoutX(RackGUI.RACK_X + RackGUI.RACK_WIDTH );
+        orderByGroup.setLayoutY(RackGUI.RACK_Y + RackGUI.RACK_HEIGHT / 2 - 150);
 
         ImageView rackIV = new ImageView(Images.rack);
         rackIV.setPreserveRatio(true);
@@ -40,13 +49,26 @@ class GameScreen extends Pane {
         rackIV.setLayoutY(888);
         //System.out.println(rackIV.getLayoutBounds().getHeight());
 
-        getChildren().addAll(players, gameboard, rackIV, rack, endTurn);
+        getChildren().addAll(players, gameboard, rackIV, rack, endTurn, orderByGroup, orderByStairs);
 
 
         rack.handToRack(Game.getInstance().currentPlayer.getHand());
         //System.out.println(Game.getInstance().currentPlayer + "'s hand is " + Game.printBoard(Game.getInstance().currentPlayer.getHand()));
         //System.out.println(Game.printBoard(GameboardGUI.getInstance().getState()));
 
+        orderByStairs.setOnAction(e -> {
+            System.out.println("okay dude");
+            Game.orderRackByStairs(Game.getInstance().currentPlayer.getHand());
+            rack.handToRack(Game.getInstance().currentPlayer.getHand());
+        });
+        
+        orderByGroup.setOnAction(e -> {
+            System.out.println("okay dude");
+            Game.orderRackByStairs(rack.handToRack(Game.getInstance().currentPlayer.getHand()));
+            Game.orderRackByGroup(rack.getInstance().tiles);
+            
+            rack.handToRack(Game.getInstance().currentPlayer.getHand());
+        });
 
         endTurn.setOnAction(e -> {
             //System.out.println("Current player: " + Game.getInstance().currentPlayer);
