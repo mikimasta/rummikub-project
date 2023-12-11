b1736ccb9f6ff86ce3f1ef8de99a80272cef6e2f
MiriamEspinosa <miriam.espinosa@student.maastrichtuniversity.nl>
2023-12-11 14:56:00 +0100

    Iterative Baseline agent

--- src/main/java/com/rummikub/game/AI_NB.java
+++ src/main/java/com/rummikub/game/AI_NB.java
@@ -1,7 +1,6 @@
 package com.rummikub.game;
 
 import java.util.ArrayList;
-import javafx.scene.paint.Color;
 
 public class AI_NB {
 
@@ -24,6 +23,8 @@ public class AI_NB {
             newRack = (ArrayList<Tile>) baselineResult[1];
             count = (int) baselineResult[2];
             moves.addAll((ArrayList<Move>)baselineResult[3]);
+            System.out.println("new board");
+            System.out.println(Game.boardListToSetKey(newBoard));
 
         } while (count != 0);
 
@@ -33,103 +34,7 @@ public class AI_NB {
         }
         System.out.println("Final board");
         System.out.println(Game.boardListToSetKey(newBoard));
-        System.out.println("Possible moves");
-        System.out.println(moves);
-    }
 
-    public static void main(String[] args) {
-        ArrayList<Tile> set1 = new ArrayList<>();
-        Tile tile1 = new Tile((byte) 1, Color.RED);
-        Tile tile2 = new Tile((byte) 2,Color.RED);
-        Tile tile3 = new Tile((byte) 3, Color.RED);
-        set1.add(tile1);
-        set1.add(tile2);
-        set1.add(tile3);
-
-        ArrayList<Tile> set2 = new ArrayList<>();
-
-        Tile tile4 = new Tile((byte) 1, Color.ORANGE);
-        Tile tile5 = new Tile((byte) 2,Color.ORANGE);
-        Tile tile6 = new Tile((byte) 3, Color.ORANGE);
-        set2.add(tile4);
-        set2.add(tile5);
-        set2.add(tile6);
-
-        ArrayList<Tile> set3 = new ArrayList<>();
-        Tile tile7 = new Tile((byte) 3, Color.RED);
-        Tile tile8 = new Tile((byte) 4,Color.RED);
-        Tile tile9 = new Tile((byte) 5, Color.RED);
-        set3.add(tile7);
-        set3.add(tile8);
-        set3.add(tile9);
-
-        ArrayList<Tile> set4 = new ArrayList<>();
-        Tile tile10 = new Tile((byte) 3, Color.BLACK);
-        Tile tile11 = new Tile((byte) -1,Color.BROWN);
-        Tile tile12 = new Tile((byte) 5, Color.BLACK);
-        set4.add(tile10);
-        set4.add(tile11);
-        set4.add(tile12);
-
-        ArrayList<Tile> set5 = new ArrayList<>();
-        Tile tile13 = new Tile((byte) 7, Color.BLACK);
-        Tile tile14 = new Tile((byte) 8,Color.BLACK);
-        Tile tile15 = new Tile((byte) 9, Color.BLACK);
-        Tile tile16 = new Tile((byte) 10, Color.BLACK);
-        Tile tile17 = new Tile((byte) 11, Color.BLACK);
-        Tile tile18 = new Tile((byte) 12, Color.BLACK);
-        set5.add(tile13);
-        set5.add(tile14);
-        set5.add(tile15);
-        set5.add(tile16);
-        set5.add(tile17);
-        set5.add(tile18);
-
-        ArrayList<Tile> set6 = new ArrayList<>();
-        Tile tile19 = new Tile((byte) 7, Color.RED);
-        Tile tile20 = new Tile((byte) 7,Color.BLACK);
-        Tile tile21 = new Tile((byte) 7, Color.BLUE);
-        set6.add(tile19);
-        set6.add(tile20);
-        set6.add(tile21);
-
-        ArrayList<Tile> set7 = new ArrayList<>();
-        Tile tile22 = new Tile((byte) 11, Color.RED);
-        Tile tile23 = new Tile((byte) 11,Color.BLACK);
-        Tile tile24 = new Tile((byte) 11, Color.BLUE);
-        Tile tile25 = new Tile((byte) 11, Color.ORANGE);
-
-        set7.add(tile22);
-        set7.add(tile23);
-        set7.add(tile24);
-        set7.add(tile25);
-
-
-        ArrayList<ArrayList<Tile>> fullBoard = new ArrayList<>();
-        fullBoard.add(set1);
-        fullBoard.add(set2);
-        fullBoard.add(set3);
-        fullBoard.add(set4);
-        fullBoard.add(set5);
-        fullBoard.add(set6);
-        fullBoard.add(set7);
-
-
-        ArrayList<Tile> hand = new ArrayList<>();
-        Tile tile26 = new Tile((byte) 4, Color.RED);
-        Tile tile27 = new Tile((byte) 11, Color.ORANGE);
-        Tile tile28 = new Tile((byte) 7, Color.ORANGE);
-        Tile tile29 = new Tile((byte) 5, Color.BLACK);
-        Tile tile30 = new Tile((byte) 13, Color.BLUE);
-        Tile tile31 = new Tile((byte) 6, Color.BLACK);
-
-        hand.add(tile26);
-        hand.add(tile27);
-        hand.add(tile28);
-        hand.add(tile31);
-        hand.add(tile30);
-        hand.add(tile29);
-        possibleMoves(hand,fullBoard);
     }
 
 
--- src/main/java/com/rummikub/game/BaselineAgentNB.java
+++ src/main/java/com/rummikub/game/BaselineAgentNB.java
@@ -3,12 +3,13 @@ package com.rummikub.game;
 
 import javafx.scene.paint.Color;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.ArrayList;
 
 public class BaselineAgentNB {
 
-    public static Object[] baselineAgentNB(ArrayList<Tile> hand, ArrayList<String> board){
+    public static Object[] baselineAgentNB(ArrayList<Tile> hand, ArrayList<String> board) {
         Object[] result = new Object[4];
         int count = 0;
         ArrayList<ArrayList<Tile>> newboard = new ArrayList<>();
@@ -16,92 +17,147 @@ public class BaselineAgentNB {
 
         Iterator<String> boardIterator = board.iterator();
         while (boardIterator.hasNext()) {
+            int countset = 0;
             String set = boardIterator.next();
             ArrayList<Tile> setTile = Game.orderSetStairs(ValidSets.getSetForKey(set));
-            ArrayList<Tile> setNB = ValidSetsNB.getNBForKey(set);
             ArrayList<Tile> createNewSet = new ArrayList<>();
-            if(setTile.size()>3){ //Intentar coger dos tiles de rack creando un nuevo set con una tile de board
-                if(set.startsWith("-1None")){
-                    Tile joker = Game.orderSetStairs(setTile).get(0);
-                    if(!getTilesWithConsecutiveNumbers(hand).isEmpty()) {
-                        Tile t1 = getTilesWithConsecutiveNumbers(hand).get(0);
-                        Tile t2 = getTilesWithConsecutiveNumbers(hand).get(1);
-                        createNewSet.add(t1);hand.remove(t1);
-                        createNewSet.add(t2);hand.remove(t2);
-                        createNewSet.add(joker);setTile.remove(joker);
-                        newboard.add(setTile);newboard.add(createNewSet);
-                        break;
-                    } else if(!getTilesWithSameNumbers(hand).isEmpty()){
-                        Tile t1 = getTilesWithSameNumbers(hand).get(0);
-                        Tile t2 = getTilesWithSameNumbers(hand).get(1);
-                        createNewSet.add(t1); hand.remove(t1);
-                        createNewSet.add(t2); hand.remove(t2);
-                        createNewSet.add(joker); setTile.remove(joker);
-                        newboard.add(setTile); newboard.add(createNewSet);
-                        break;
+            ArrayList<Tile> setNB = ValidSetsNB.getNBForKey(set);
+            //REARRANGEMENT
+            if (setTile.size()>3) {
+                if(Game.checkIfStairs(setTile)){
+                    Tile first = setTile.get(0);
+                    Tile last = setTile.get(setTile.size()-1);
+                    if(addPartialRun(first, hand)!= null){
+                        createNewSet = addPartialRun(first, hand);
+                        newboard.add(createNewSet);
+                        hand.remove(createNewSet.get(0));
+                        hand.remove(createNewSet.get(1));
+                        setTile.remove(first);
+                        newboard.add(setTile);
+                        count += 2;
+                        countset++;
+                    }else if(addPartialRun(last, hand)!=null){
+                        createNewSet = addPartialRun(last, hand);
+                        newboard.add(createNewSet);
+                        hand.remove(createNewSet.get(0));
+                        hand.remove(createNewSet.get(1));
+                        setTile.remove(last);
+                        newboard.add(setTile);
+                        count += 2;
+                        countset++;
+
+                    }else if(addPartialGroup(first, hand) != null){
+                        createNewSet = addPartialGroup(first, hand);
+                        newboard.add(createNewSet);
+                        hand.remove(createNewSet.get(0));
+                        hand.remove(createNewSet.get(1));
+                        setTile.remove(first);
+                        newboard.add(setTile);
+                        count += 2;
+                        countset++;
+
+                    }else if(addPartialGroup(last, hand) != null){
+                        createNewSet = addPartialGroup(last, hand);
+                        newboard.add(createNewSet);
+                        hand.remove(createNewSet.get(0));
+                        hand.remove(createNewSet.get(1));
+                        setTile.remove(last);
+                        newboard.add(setTile);
+                        count += 2;
+                        countset++;
                     }
                 }else if(Game.checkIfGroup(setTile)){
-                    ArrayList<Tile> consecutive = getTilesWithConsecutiveNumbers(hand);
-                    if(!consecutive.isEmpty()) {
-                        for (int i = 0; i < consecutive.size() ; i += 2) {
-                            if(consecutive.get(i).getNumber()==setTile.get(0).getNumber()){
-                                Tile t1 = getTilesWithConsecutiveNumbers(hand).get(i);
-                                Tile t2 = getTilesWithConsecutiveNumbers(hand).get(i+1);
-                                for (int j = 0; j < 4; j++) {
-                                    if((setTile.get(i).getColor()!=t1.getColor())&&(setTile.get(i).getColor()!=t2.getColor())) {
-                                        createNewSet.add(t1);
-                                        hand.remove(t1);
-                                        createNewSet.add(t2);
-                                        hand.remove(t2);
-                                        createNewSet.add(setTile.get(i));
-                                        setTile.remove(setTile.get(i));
-                                        newboard.add(setTile);
-                                        newboard.add(createNewSet);
-                                        break;
-                                    }
-                                }
-                            }
+                    for(Tile selected: setTile){
+                        //System.out.println(selected.getNumber()+" "+ Game.getStringFromColor(selected));
+                        if(addPartialRun(selected, hand)!= null){
+                            createNewSet = addPartialRun(selected, hand);
+                            newboard.add(createNewSet);
+                            hand.remove(createNewSet.get(0));
+                            hand.remove(createNewSet.get(1));
+                            setTile.remove(selected);
+                            newboard.add(setTile);
+                            count += 2;
+                            countset++;
+                            break;
+
+                        }else if(addPartialGroup(selected, hand) != null) {
+                            createNewSet = addPartialGroup(selected, hand);
+                            newboard.add(createNewSet);
+                            hand.remove(createNewSet.get(0));
+                            hand.remove(createNewSet.get(1));
+                            setTile.remove(selected);
+                            newboard.add(setTile);
+                            count += 2;
+                            countset++;
+                            break;
                         }
-
-                        newboard.add(setTile);
+                    }
+                }
+            }
+            //IF THE SET STARTS WITH JOCKER
+            if (set.startsWith("-1None") &&(setTile.size()>3)) {
+                ArrayList<Tile> setOrder = Game.orderSetStairs(setTile);
+                Tile joker = setOrder.get(setTile.size()-1);
+                if((setOrder.get(setOrder.size()-2).getNumber()== setOrder.get(setOrder.size()-3).getNumber())&&(setOrder.get(setOrder.size()-3).getNumber() == setOrder.get(setOrder.size()-4).getNumber())){
+                    ArrayList<Tile> consecutive = getTilesWithConsecutiveNumbers(hand);
+                    ArrayList<Tile> sameNum = getTilesWithSameNumbers(hand);
+                    if(!consecutive.isEmpty()){
+                        createNewSet.add(consecutive.get(0));
+                        createNewSet.add(consecutive.get(1));
+                        createNewSet.add(joker);
+                        hand.remove(createNewSet.get(0));
+                        hand.remove(createNewSet.get(1));
+                        setTile.remove(joker);
                         newboard.add(createNewSet);
-                        break;
-                    }
-                } else if(Game.checkIfStairs(setTile)) {
-
-                }
-
-
-            }
-                for(Tile NB : setNB){
-                Iterator<Tile> handIterator = hand.iterator();
-                while (handIterator.hasNext()) {
-                    Tile tileHand = handIterator.next();
-                    if(NB==null){
-                        break;
-                    }
-                    if ((NB.getColor()==tileHand.getColor())&&(NB.getNumber()== tileHand.getNumber())) { //there is a Match
-                        ArrayList<Tile> tileAdded = new ArrayList<>();
-                        tileAdded.add(tileHand);
-                        Move move = new Move(setTile, tileAdded);
-                        setTile.add(tileHand);
-                        hand.remove(tileHand);
-                        moves.add(move);
-                        count++;
-                        break;
+                        newboard.add(setTile);
+                        count += 2;
+                        countset++;
+                    }else if(!sameNum.isEmpty()){
+                        createNewSet.add(sameNum.get(0));
+                        createNewSet.add(sameNum.get(1));
+                        createNewSet.add(joker);
+                        hand.remove(createNewSet.get(0));
+                        hand.remove(createNewSet.get(1));
+                        setTile.remove(joker);
+                        newboard.add(createNewSet);
+                        newboard.add(setTile);
+                        count += 2;
+                        countset++;
+                    }else{
+                        newboard.add(setTile);
                     }
                 }
             }
-            setTile = Game.orderSetStairs(setTile);
-            newboard.add(setTile);
+            //SEARCH BASED ON ONE NB
+            if(countset==0) {
+                for (Tile NB : setNB) {
+                    Iterator<Tile> handIterator = hand.iterator();
+                    while (handIterator.hasNext()) {
+                        Tile tileHand = handIterator.next();
+                        if (NB == null) {break;}
+                        if ((NB.getColor() == tileHand.getColor()) && (NB.getNumber() == tileHand.getNumber())) { //there is a Match
+                            ArrayList<Tile> tileAdded = new ArrayList<>();
+                            tileAdded.add(tileHand);
+                            Move move = new Move(setTile, tileAdded);
+                            setTile.add(tileHand);
+                            hand.remove(tileHand);
+                            moves.add(move);
+                            count++;
+                            setTile = Game.orderSetStairs(setTile);
+                            break;
+                        }
+                    }
+                }
+                newboard.add(setTile);
+            }
         }
-
         result[0] = newboard;
         result[1] = hand;
         result[2] = count;
         result[3] = moves;
         return result;
     }
+
 
     public static ArrayList<Tile> getTilesWithSameNumbers(ArrayList<Tile> tiles) {
         tiles = Game.orderSetGroup(tiles);
@@ -119,7 +175,7 @@ public class BaselineAgentNB {
 
     public static ArrayList<Tile> getTilesWithConsecutiveNumbers(ArrayList<Tile> tiles) {
         tiles = Game.orderSetStairs(tiles);
-        System.out.println(tiles);
+        //System.out.println(tiles);
         ArrayList<Tile> resultTiles = new ArrayList<>();
         for (int i = 0; i < tiles.size() - 1; i++) {
             int currentNumber = tiles.get(i).getNumber();
@@ -130,6 +186,42 @@ public class BaselineAgentNB {
             }
         }
         return resultTiles;
+    }
+
+    public static ArrayList<Tile> addPartialGroup(Tile tile, ArrayList<Tile> hand) {
+        int num = tile.getNumber();
+        ArrayList<Tile> sameNum = getTilesWithSameNumbers(hand);
+        ArrayList<Tile> newSet = new ArrayList<>();
+        if (!sameNum.isEmpty()) {
+            for (int i = 0; i < sameNum.size(); i += 2) {
+                boolean condition1 = (sameNum.get(i).getNumber() == num);
+                boolean condition2 = (((sameNum.get(i).getColor()) == tile.getColor()) && ((sameNum.get(i + 1).getColor()) == tile.getColor()));
+                if (condition1 && condition2) {
+                    newSet.add(sameNum.get(i));
+                    newSet.add(sameNum.get(i + 1));
+                    newSet.add(tile);
+                    return newSet;
+                }
+            }
+        }
+        return null;
+    }
+    public static ArrayList<Tile> addPartialRun(Tile tile, ArrayList<Tile> hand) {
+        ArrayList<Tile> consecutive = getTilesWithConsecutiveNumbers(hand);
+        ArrayList<Tile> newSet = new ArrayList<>();
+        if (!consecutive.isEmpty()) {
+            for (int i = 0; i < consecutive.size(); i += 2) {
+                boolean condition1 = ((consecutive.get(i).getNumber()) == tile.getNumber()+1) && (consecutive.get(i).getColor() == tile.getColor());
+                boolean condition2 = ((consecutive.get(i).getNumber() + 2) == tile.getNumber()) && (consecutive.get(i).getColor() == tile.getColor());
+                if (condition1 || condition2) {
+                    newSet.add(consecutive.get(i));
+                    newSet.add(consecutive.get(i + 1));
+                    newSet.add(tile);
+                    return newSet;
+                }
+            }
+        }
+        return null;
     }
 
 }
