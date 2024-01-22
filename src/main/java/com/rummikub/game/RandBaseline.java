f082a4fd2bdd1d2c434a6ae37bb0b3ebd8f895c1
m-drapers <milou.a.drapers@gmail.com>
2024-01-22 18:16:53 +0100

    Random Baseline

---
+++ src/main/java/com/rummikub/game/RandBaseline.java
@@ -0,0 +1,21 @@
+package com.rummikub.game;
+
+import java.util.ArrayList;
+
+import com.rummikub.Utils;
+import com.rummikub.movegen.MoveGenerator;
+
+public class RandBaseline {
+    public static ArrayList<ArrayList<Tile>> getRandMove(ArrayList<ArrayList<Tile>> stateBoard, ArrayList<Tile> stateHand){
+        ArrayList<ArrayList<Tile>> stateHand2 = new ArrayList<>();
+        stateHand2.add(stateHand);
+        ArrayList<ArrayList<ArrayList<Tile>>> solutions = MoveGenerator.getInstance().getSolutions(Utils.listTo2dArray(stateBoard), Utils.listTo2dArray(stateHand2));
+        
+        int randomNumber = (int) (Math.random() * (solutions.size()-1));
+        return solutions.get(randomNumber);
+
+
+    }
+}
+
+
