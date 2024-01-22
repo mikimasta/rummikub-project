4b1db1fbddbe84f6c8aa3bd577e2b82db6dcd8ab
m-drapers <milou.a.drapers@gmail.com>
2024-01-22 09:54:31 +0100

    simulation fix

--- src/main/java/com/rummikub/MCTS/MCTS.java
+++ src/main/java/com/rummikub/MCTS/MCTS.java
@@ -80,10 +80,6 @@ public class MCTS {
         return node;
     }
 
-//    // private  ArrayList<ArrayList<ArrayList<Tile>>> generateValidMoves(ArrayList<ArrayList<Tile>> stateBoard, ArrayList<Tile> stateHand){
-//         ArrayList<ArrayList<ArrayList<Tile>>> validMoves = new ArrayList<ArrayList<ArrayList<Tile>>>();
-//         //fill this in
-//     //}
 
     private  ArrayList<Tile> getNewHand(ArrayList<ArrayList<Tile>> solutionBoard, ArrayList<ArrayList<Tile>> intialBoard, ArrayList<Tile> hand){
         ArrayList<Tile> usedTiles = new ArrayList<>();
@@ -139,12 +135,24 @@ public class MCTS {
         initalBoard = stateBoard;
         while(stateHand != null && opponentHand != null){
             if(player == 0){
-                stateBoard = Move.getMove(stateHand, stateBoard);
+                ArrayList<ArrayList<Tile>> move = Move.getMove(stateHand, stateBoard);
+                if(move != null){
+                stateBoard = move;
                 stateHand = getNewHand(initalBoard, stateBoard, stateHand);
+                }
+                else{
+                    stateHand.add(pile.remove(0));
+                }
             }
             else if(player == 1){
-                stateBoard = Move.getMove(opponentHand, stateBoard);
+                ArrayList<ArrayList<Tile>> move = Move.getMove(opponentHand, stateBoard);
+                if(move != null){
+                stateBoard = move;
                 opponentHand = getNewHand(initalBoard, stateBoard, opponentHand);
+                }
+                else{
+                    opponentHand.add(pile.remove(0));
+                }
             }
         }
 
--- src/main/java/com/rummikub/ML/getPrediction.py
+++ src/main/java/com/rummikub/ML/getPrediction.py
@@ -1,9 +1,6 @@
 import numpy as np
 
 def getPrediction(Theta1, Theta2, X):
-#PREDICT Predict the label of an input given a trained neural network
-#   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
-#   trained weights of a neural network (Theta1, Theta2)
 
 # Useful values
     m = np.shape(X)[0]              #number of examples
@@ -11,11 +8,6 @@ def getPrediction(Theta1, Theta2, X):
 # You need to return the following variables correctly 
     p = np.zeros(m);
 
-# ====================== YOUR CODE HERE ======================
-# Instructions: Complete the following code to make predictions using
-#               your learned neural network. You should set p to a 
-#               vector containing labels between 1 to num_labels.
-#
     # add a column of 1s to X for the bias term
     X = np.column_stack((np.ones((m, 1)), X))
     
@@ -33,13 +25,8 @@ def getPrediction(Theta1, Theta2, X):
     # make predictions based on the highest activation
     p = np.argmax(a3, axis=1) + 1  # +1 because the labels are 1-indexed
     
-
-# =========================================================================
     return p
 
 def sigmoid(z):
-#SIGMOID Compute sigmoid functoon
-#   J = SIGMOID(z) computes the sigmoid of z.
-
     g = 1/(1 + np.exp(-z))
     return g
